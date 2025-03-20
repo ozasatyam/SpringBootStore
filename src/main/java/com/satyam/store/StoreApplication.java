302afd162b859587ef692a464b6abb04cc893bca
ozasatyam <satyamoza@outlook.com>
2025-03-19 23:52:36 -0600

    starter

--- src/main/java/com/satyam/store/StoreApplication.java
+++ src/main/java/com/satyam/store/StoreApplication.java
@@ -2,8 +2,10 @@ package com.satyam.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.satyam.store")
 public class StoreApplication {
 
 	public static void main(String[] args) {
